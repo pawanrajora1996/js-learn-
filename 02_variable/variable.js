3c8acec996c5f3fb907e4badae9083a15d6f21f7
pawanrajora1996 <111762989+pawanrajora1996@users.noreply.github.com>
2023-08-28 16:52:01 +0000

    var const test

---
+++ 02_variable/variable.js
@@ -0,0 +1,15 @@
+
+const accoutnId = 12345
+let accountPassword="pawan"
+var accountEmail = "pawan@google.com"
+accoutnCity = "jaipur"
+
+accountId = 2112211
+accountPassword = "rajora"
+accountEmail = "rajora@google.com"
+accoutnCity = "dubai"
+
+
+
+
+console.table([accoutnId, accountEmail,accountPassword, accoutnCity])
